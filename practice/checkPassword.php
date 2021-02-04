8828b4c665f0c9e6f1683b2d550eb397379e0367
roshani sahu <roshani2203@gmail.com>
2021-02-04 17:30:01 +0530

    project

---
+++ practice/checkPassword.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+      1.Retrieve post data : "uname" and "pass".
+      2.Cehck if  user name and password is valid.
+    */
+    if( /* valid user name and password */ ){
+        echo "OK";
+    }else{
+        echo "NG";
+    }
+?>
